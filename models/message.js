13bea157aa3341298f55add219a67cb923026452
Lisa Fink <lisadfink@gmail.com>
2022-10-21 17:04:17 -0400

    Changed username to have lowercase n

--- models/message.js
+++ models/message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  userName: String,
+  username: String,
   message: String,
   title: String,
   date: Date,
